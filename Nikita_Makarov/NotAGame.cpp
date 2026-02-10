85996b6fee122623fc342fe45828f6fb9ec6921b
Stug-7v <makarovnik@icloud.com>
2026-02-10 21:34:15 +0300

    Add files via upload

---
+++ Nikita_Makarov/NotAGame.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// typedef int RUB; // C
+
+//using AUF = long long int; // C++ printf("Salary = %lld\n")
+//using RUB = long int; // C++ printf("Salary = %ld\n")
+//using EUR = int; // C++ printf("Salary = %d\n")
+
+// M.Gundy from Civilisatoin 2
+// && == and
+
+using DAMAGE = int;
+using HP = int;
+
+struct Weapon {
+    DAMAGE sword;
+    DAMAGE lance;
+    DAMAGE tooths;
+};
+
+struct Person hero;
+struct Person enemy;
+
+void hero_init(){
+    hero.sword = 50;
+    hero.lance = 75;
+    hero.tooths = 32;
+}
+
+int main(){
+    hero_init;
+}
+
+//I dont understand anything:(((
+
+
+
+
