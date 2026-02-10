24b17b9cce0273567f9aa6dbe28ebe5c04dea323
NikBudnik <yauskiy@gmail.com>
2026-02-10 12:34:39 +0300

    update

--- Budnik_Nikolay/claswork1.cpp
+++ Budnik_Nikolay/claswork1.cpp
@@ -5,6 +5,11 @@ using RUB = long long int; // C++
 
 RUB salary = 180001;
 RUB capital = 0;
+
+void print(){
+    printf("Salary = %lld\n", salary);
+    printf("Capital = %lld", capital);
+}
 void my_salary(const int month, const int year) {
   if ((month == 8) && (year == 2026)) { // Promotion
     salary = salary * 1.5;
@@ -12,26 +17,28 @@ void my_salary(const int month, const int year) {
   capital += salary;
 }
 
+void simulation(){
+    float pp = 14.5;
+
+    int month = 2;
+    int year = 2026;
+
+  //  for (int month = 1; month < 19; ++month) {
+    while (!((month == 3) && (year == 2027))) {
+      capital += capital * (pp / 12.0 / 100.0); // Bank interest
+
+      my_salary(month, year);
+
+      ++month;
+      if (month == 13) {
+        month = 1;
+        ++year;
+      }
+    }
+}
+
 int main()
 {
-  float pp = 14.5;
-
-  int month = 2;
-  int year = 2026;
-
-//  for (int month = 1; month < 19; ++month) {
-  while (!((month == 3) && (year == 2027))) {
-    capital += capital * (pp / 12.0 / 100.0); // Bank interest
-
-    my_salary(month, year);
-
-    ++month;
-    if (month == 13) {
-      month = 1;
-      ++year;
-    }
-  }
-
-  printf("Salary = %lld\n", salary);
-  printf("Capital = %lld", capital);
+    simulation();
+    print();
 }
