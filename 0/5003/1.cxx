158556d438027e084cea598fbba0381503f7bb33
soomrack <soomrack@gmail.com>
2026-02-10 15:30:40 +0300

    result 5003

--- 0/5003/1.cxx
+++ 0/5003/1.cxx
@@ -1,38 +1,99 @@
 #include <stdio.h>
 
-int salary = 100'001;
-int capital = 0;
-int food = 15'000;
+
+// typedef int RUB;  // C
+using RUB = long long int;  // C++
 
 
-void my_salary(const int month)
+struct Car {
+    RUB value;
+    RUB gas;
+};
+
+
+struct Person {
+    RUB salary;
+    RUB capital;
+    RUB food;
+    Car car;
+};
+struct Person alice;
+
+
+void alice_init()
 {
-    if (month == 3) {
-        salary *= 1.5;
+    alice.capital = 0;
+    alice.salary = 180'000;
+    alice.food = 15'000;
+
+    alice.car.value = 2'400'000;
+    alice.car.gas = 12'000;
+}
+
+
+float sberbank_interest(RUB sum)
+{
+    if (sum > 10'000 and sum < 3'000'000) {
+        return 11.5;
+    }
+
+    if (sum >= 3'000'000) {
+        return 12.5;  //  125 -1 +
+    }
+
+    return 0.1;
+}
+
+
+void alice_salary(const int year, const int month)
+{
+    if (month == 3 and year == 2026) {
+        alice.salary *= 1.5;
     }
         
-    capital += salary;
+    alice.capital += alice.salary;
 }
 
 
-void my_food()
+void alice_food()
 {
-    capital -= food;
+    alice.capital -= alice.food;
 }
 
 
-int main()
+void print_results()
+{
+    printf("Salary = %lld\n", alice.salary);
+    printf("Capital = %lld", alice.capital);
+}
+
+
+void alice_car()
+{
+    alice.capital -= alice.car.gas;
+}
+
+
+void alice_bank_interest()
+{
+    float pp;
+    pp = sberbank_interest(alice.capital);
+    alice.capital += alice.capital * (pp / 12.0 / 100.0);
+}
+
+
+void alice_simulation()
 {
     int year = 2026;
     int month = 2;
     
-    while ( !(year == 2027 && month == 2) ) {
+    while ( not (year == 2027 and month == 2) ) {
         // my_cat();
         // my_trip();
-        my_food();
-        // my_car();
-        // my_bank_interest();
-        my_salary(month);
+        alice_food();
+        alice_car();
+        alice_bank_interest();
+        alice_salary(year, month);
         
         ++month;
         if (month == 13) {
@@ -40,7 +101,14 @@ int main()
             month = 1;
         }
     }
+}
+
+
+int main()
+{
+    alice_init();
     
-    printf("Salary = %d\n", salary);
-    printf("Capital = %d", capital);
+    alice_simulation();
+    
+    print_results();
 }
