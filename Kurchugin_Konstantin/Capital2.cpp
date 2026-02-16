a9b98edc142791c9fc91e32cc49e08db31d9ee89
rtxsimon <kkurchugin@mail.ru>
2026-02-16 12:23:59 +0300

    Update Capital2.cpp

--- Kurchugin_Konstantin/Capital2.cpp
+++ Kurchugin_Konstantin/Capital2.cpp
@@ -1,109 +1,67 @@
 #include <stdio.h>
 
-using RUB = long long int;
+//typedef int RUB; // Ключевое слово, зарезервировано под функции языка, используют в С
+using RUB = long long int; // Используют в С++, можно из одного места поменять типы данных многих переменных
 
 struct Car {
   RUB value;
   RUB gas;
 };
 
-struct Pet {
-  RUB food;
-};
-
-struct Bank {
-  RUB account;   
-  RUB deposit;   
-};
-
-struct Person {
+struct Person { // создали свой тип данных
   RUB salary;
-  RUB capital;   
+  RUB capital;
   RUB food;
-
   Car car;
-  Pet pet;
-  Bank bank;
 };
 
-struct Person Alice;
+struct Person Alice; // создали переменную типа данных Person
 
-float bank_percent = 14.5;      
-float inflation = 8.0;          
-
-void alice_salary(const int month, const int year) {
-
-  if ((month == 8) && (year == 2026)) { 
-    Alice.salary = Alice.salary * 1.5;
-  }
-
-  
-  Alice.bank.account += Alice.salary;
-}
-
-void alice_food() {
-  Alice.bank.account -= Alice.food;
-}
+float pp = 14.5;
 
 void alice_car() {
-  Alice.bank.account -= Alice.car.gas;
+  Alice.capital -= Alice.car.gas;
 }
 
-void alice_pet() {
-  Alice.bank.account -= Alice.pet.food;
-}
-
-void alice_deposit_percent() {
-  
-  Alice.bank.deposit += Alice.bank.deposit * (bank_percent / 12.0 / 100.0);
-}
-
-void alice_transfer_to_deposit() {
-  
-  if (Alice.bank.account > 100000) {
-    RUB sum = Alice.bank.account - 100000;
-    Alice.bank.deposit += sum;
-    Alice.bank.account -= sum;
+void alice_salary(const int month, const int year) {
+  if ((month == 8) && (year == 2026)) { // Promotion
+    Alice.salary = Alice.salary * 1.5;
   }
-}
-
-void alice_inflation(int month) {
-
-  
-  if (month == 1) {
-    Alice.food += Alice.food * (inflation / 100.0);
-    Alice.car.gas += Alice.car.gas * (inflation / 100.0);
-    Alice.pet.food += Alice.pet.food * (inflation / 100.0);
-    Alice.car.value += Alice.car.value * (inflation / 100.0);
-  }
+  Alice.capital += Alice.salary;  
 }
 
 void print_results() {
 
   printf("Salary = %lld\n", Alice.salary);
-  printf("Bank account = %lld\n", Alice.bank.account);
-  printf("Deposit = %lld\n", Alice.bank.deposit);
-  printf("Car price = %lld\n", Alice.car.value);
+  printf("Capital = %lld", Alice.capital);
+}
+
+void alice_food() {
+  Alice.capital -= Alice.food;
+}
+
+void alice_bank_interest() {
+
+
 }
 
 void simulation()
 {
+
   int month = 2;
   int year = 2026;
 
+  //  for (int month = 1; month < 19; ++month) {
   while (!((month == 3) && (year == 2127))) {
+    Alice.capital += Alice.capital * (pp / 12.0 / 100.0); // Bank interest
 
-    alice_inflation(month);
 
-    alice_deposit_percent();
 
+    // my_cat()
+    // my_trip()
     alice_food();
     alice_car();
-    alice_pet();
-
     alice_salary(month, year);
-
-    alice_transfer_to_deposit();
 
     ++month;
     if (month == 13) {
@@ -113,25 +71,26 @@ void simulation()
   }
 }
 
-void alice_init()
+void alice_init() // Присвоили значения характеристик, описывающих переменную данного типа
 {
-  Alice.salary = 180000;
   Alice.capital = 0;
-
+  Alice.salary = 180000;
   Alice.food = 20000;
 
   Alice.car.value = 2400000;
   Alice.car.gas = 15000;
-
-  Alice.pet.food = 5000;
-
-  Alice.bank.account = 0;
-  Alice.bank.deposit = 0;
 }
+
 
 int main()
-{
+{ 
   alice_init();
+
   simulation();
+  
   print_results();
+
 }
+
+
+// long int - int хранит 4 байта, а не 2, в принте надо менять отдельно
