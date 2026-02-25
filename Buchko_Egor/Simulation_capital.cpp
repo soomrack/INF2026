c502a1567ade367877733817ced895fe5e6976f2
Егор <119059728+Egor-Buchko@users.noreply.github.com>
2026-02-25 22:32:06 +0300

    Update Simulation_capital.cpp
    add Steam akk

--- Buchko_Egor/Simulation_capital.cpp
+++ Buchko_Egor/Simulation_capital.cpp
@@ -4,21 +4,28 @@
 using RUB = long long int;
 using USD = long long int;
 
-//расходы на авто
+// расходы на авто
 struct Car { 
     RUB value;
     RUB gas;
     RUB wash;
 };
 
-//банковский счет
+// банковский счет
 struct Bank {
     RUB account_rub;
     USD account_usd;
     float rate_usd_rub;
 };
 
-//подписки на онлайн платформы
+// игровой аккаунт
+struct Steam {
+    RUB account_rub;
+    RUB salary_steam;
+    RUB expenses_steam;
+};
+
+// подписки на онлайн платформы
 struct Sub {
     RUB yandex_music;
     RUB vpn;
@@ -26,9 +33,10 @@ struct Sub {
     USD spotify;
 };
 
-//все составляющие капитала человека
+// все составляющие капитала человека
 struct Person {
     Bank vtb;
+    Steam steam;
     RUB capital;
     RUB salary_job;
     RUB food;
@@ -38,12 +46,13 @@ struct Person {
 };
 struct Person alice;
 
-//иницилизация глобальных данных
+// иницилизация данных
 void alice_init()
 {
     alice.vtb.rate_usd_rub = 78.76;
     alice.vtb.account_rub = 0;
     alice.vtb.account_usd = 0;
+    alice.steam.account_rub = 1000;
 
     alice.salary_job = 100'000;
     alice.food = 7'000;
@@ -58,7 +67,38 @@ void alice_init()
     alice.sub.spotify = 5.5;
 }
 
-//траты на подписки
+// Steam
+void alice_steam(const int month)
+{
+    // рандомный заработок берется от кейсов в кс которые раздают каждую неделю
+    // цена кейсов может варироватся примерно от 5 до 1000 рублей
+    int random_salary = rand()%100;
+
+    if (random_salary % 9 == 0){
+        random_salary *= 10;
+    }
+    
+    // рандомные траты берутся от пакупки игр, зимой и летом скидки, 
+    // + к этому добавляются праздники или отпуск поэтому траты больше
+    int random_expenses = rand()%1000;
+    
+    if (month == 1 || month == 6){
+        random_expenses *= 3;
+    }
+
+    alice.steam.salary_steam = random_salary;
+    alice.steam.expenses_steam = random_expenses;
+    alice.steam.account_rub += random_salary;
+
+    // если не хватает денег на аккаунте то надо пополнить 
+    if (alice.steam.account_rub < random_expenses){
+        alice.vtb.account_rub -= random_expenses*1.1; // комиссия при пополнение
+        alice.steam.account_rub += random_expenses;
+    }
+    alice.steam.account_rub -= random_expenses;
+}
+
+// траты на подписки
 void alice_subs()
 {
     alice.vtb.account_rub -= alice.sub.yandex_music;
@@ -76,7 +116,7 @@ void alice_food(const int month)
     alice.capital -= alice.food;
 }
 
-//траты на машину
+// траты на машину
 void alice_car(const int month)
 {
     alice.vtb.account_rub -= alice.car.gas;
@@ -85,7 +125,7 @@ void alice_car(const int month)
     }
 }
 
-//начисление зп
+// начисление зп
 void alice_salary(const int month, const int year)
 {
     if (month == 3){
@@ -97,7 +137,7 @@ void alice_salary(const int month, const int year)
     alice.vtb.account_rub += alice.salary_job;
 }
 
-//заработок с фриланса
+// заработок с фриланса
 void alice_freelance(const int month)
 {
     int time_freelance = 0;
@@ -114,7 +154,7 @@ void alice_freelance(const int month)
     alice.vtb.account_usd += salary_freelance;
 }
 
-//симуляция нескольких лет
+// симуляция нескольких лет
 void simulation(const int delta_year, const int delta_month)
 {
     int now_year = 2026;
@@ -127,6 +167,7 @@ void simulation(const int delta_year, const int delta_month)
         alice_car(month);
         alice_subs();
         alice_freelance(month);
+        alice_steam(month);
         ++month;
         if (month == 13){
             ++year;
@@ -135,11 +176,12 @@ void simulation(const int delta_year, const int delta_month)
     }  
 }
 
-
+//вывод результатов
 void print_resullts()
 {
     RUB capital = 0;
     
+
     printf("Salary = %lld\n", alice.salary_job + alice.salary_freelance);
     printf("VTB RUB = %lld\n", alice.vtb.account_rub);
     printf("VTB USD = %lld\n", alice.vtb.account_usd);
