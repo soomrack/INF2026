412d23032f21c5b97300ad22fc8ab65a6cfc98c1
mishurinskyd <mishurinsky.d@yandex.ru>
2026-05-12 16:30:06 +0300

    Add files via upload

---
+++ Mishurinsky Denis/test.cpp
@@ -0,0 +1,9 @@
+// test.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from macOS!bcusdhgvbcouzsdgvoszdugvs8z0" << endl;
+    cout << "C++ is working!" << endl;
+    return 0;
+}
